11ec8b18d2dd9ae720abe2d77e443c9532a12ad1
Bohdan Shevaniuk <s.bogdan1555@gmail.com>
2023-07-22 13:12:40 +0300

    dsds

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,3 @@
-// import LoadMore from "components/Button/Button";
 import Loader from 'components/Loader/Loader';
 import { fetchTrendingMovies } from 'components/services/moviesAPI/fetchMovies';
 import { useEffect, useState } from 'react';
@@ -9,7 +8,6 @@ import css from './Home.module.css';
 const Home = () => {
   const [movies, setMovies] = useState([]);
   const [status, setStatus] = useState(STATUS.IDLE);
-  // const [page, setPage] = useState(1);
 
   useEffect(prevPage => {
     const getTrendingMovies = async () => {
@@ -26,18 +24,11 @@ const Home = () => {
     getTrendingMovies();
   }, []);
 
-  // const onNextPage = () => {
-  // setPage(prevPage => (prevPage + 1));
-  // };
-
-  // const showButton = status === STATUS.RESOLVED;
-
   return (
     <div className={css.container}>
       <h1 className={css.title}>Trending movies of the week</h1>
       {movies.length > 0 && <MoviesList movies={movies} />}
       {status === STATUS.PENDING && <Loader />}
-      {/* {showButton && <LoadMore onClick={onNextPage} />} */}
     </div>
   );
 };
